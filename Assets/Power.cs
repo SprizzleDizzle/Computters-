f651616517b64cd5ebb62cfb50fe95664559fec8
Nathan Gazey <ngazey60@maine.edu>
2018-03-30 14:50:20 -0400

    some changes ;)

--- Assets/Power.cs
+++ Assets/Power.cs
@@ -1,50 +1,79 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Power : MonoBehaviour
 {
 
-    private float yForce;
+    private float zForce;
     private float xForce;
-    private float maxForceXp = 2000;
-    private float maxForceXm = 2000;
-    private float startPosX;
-    private float endPosX;
+    private float maxForce = 2000;
+    private float minForce = -2000;
+    private Vector3 startPos;
+    private Vector3 endPos;
     private float originX;
-    private float startRotateZ;
-  
+    private bool init = true;
+    private LineRenderer LR;
 
+    public float SENSITIVITY = 300;
 
+    void start()
+    {
+        GetComponent<Rigidbody>().useGravity = true;
+        LR = GetComponent<LineRenderer>();
+
+    }
 
     void Update()
     {
-        originX = GetComponent<Rigidbody>().position.x;
+        originX = transform.position.x;
 
         if (Input.GetMouseButtonDown(0))
         {
-            startPosX = Input.mousePosition.x;
-            startRotateZ = Input.mousePosition.z;
-           
-            
+            if (init)
+            {
+                startPos = Input.mousePosition;
+                init = false;
+            }
+
         }
+
+
+
+        ///
+        ///something similar to this to get the z value of the mouse to show on a 2d plane as rotating the shot
+        //Vector3.ProjectOnPlane(Input.mousePosition, Vector3.forward);
+        ///
+
+
+
+        //if (Input.GetMouseButton(0))
+        //{
+        //    updateLineRend();
+        //}
 
         if (Input.GetMouseButtonUp(0))
         {
-            endPosX = Input.mousePosition.x;
-            xForce = (startPosX - endPosX) * 3;
+            endPos = Input.mousePosition;
+            xForce = Mathf.Clamp((startPos.x - endPos.x) * 3, minForce, maxForce);
+            zForce = ((startPos.z - endPos.z) * SENSITIVITY);
 
-            if (xForce > 0 && xForce > maxForceXp) xForce = maxForceXp;
+            GetComponent<Rigidbody>().AddForce(new Vector3(xForce, 0f , zForce));
 
-            if (xForce < 0 && xForce < maxForceXm) xForce = maxForceXm;
-
-            GetComponent<Rigidbody>().useGravity = true;
-            GetComponent<Rigidbody>().AddForce(new Vector2(xForce, yForce));
-
-            yForce = 0;
-            startPosX = 0;
-            endPosX = 0;
-           
+            init = true;
+            zForce = 0;
+            startPos = endPos = Vector3.zero;
+            //updateLineRend();
         }
     }
+
+    //private void updateLineRend() TODO: FIX MATH
+    //{
+    //    Vector3[] newPos = new Vector3[2];
+    //    newPos[0] = transform.position;
+    //    newPos[1] = transform.position+Vector3.ProjectOnPlane(Input.mousePosition, Vector3.forward);
+
+    //    GetComponent<LineRenderer>().SetPositions(newPos);
+    //}
 }
